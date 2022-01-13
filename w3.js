308c75abf6bbd619a4902d4be778bfdb325ca8c4
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-01-13 02:15:03 -0800

    this is an external js file with functions to carryout certain activities when invoked

---
+++ w3.js
@@ -0,0 +1,15 @@
+function button(){
+    document.getElementById("demo").innerHTML="the first function has worked!";
+}
+document.write("Explore Javascript")
+window.alert("Give feedback")
+console.log("Deal with Javascript like a pro");
+/*let x, y, z;
+
+ function sum(x,y,z){
+     let x=9;
+     let y=10;
+     let z=23;
+     let sum= x+y+z;
+window.write(sum(x,y,z));
+ }*/
